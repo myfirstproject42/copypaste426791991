f7baea52e76f993b9ae611506cafe438266e4cfc
mfp <rezvin.anton@gmail.com>
2016-12-12 03:28:40 +0700

    base done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -47,6 +47,15 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-8">
+                                @if(count($errors)>0)
+                                    <div class="alert alert-danger">
+                                        <ul> 
+                                            @foreach($errors->all() as $error)
+                                                <li>{{$error}}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>>
+                                @endif
                                 <h1>Добавить текст/код</h1>
                                 <form method="POST" action="{{route('addPaste')}}">  
                                     <label for="description" class="control-label">Код/текст</label>
